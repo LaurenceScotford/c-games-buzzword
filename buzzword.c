1b973289dc0c57b48bfa9f4a093312c9a00ff20e
Laurence Scotford <laurencescotford@gmail.com>
2020-08-05 07:04:47 +0100

    Create buzzword.c
    Create "Hello, world!" starter file.

---
+++ buzzword.c
@@ -0,0 +1,14 @@
+/*******
+ * Buzzword
+ *
+ * A program to create amusing fake jargon
+ *******/
+
+ #include <stdio.h>
+
+ int main(int argc, const char * argv[])
+ {
+   printf("Hello, World!\n");
+   return 0;
+ }
+ 
