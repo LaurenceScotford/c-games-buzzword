2cdd45cd255e82b5baba9d0d286e9a589a2cea5b
Laurence Scotford <laurencescotford@gmail.com>
2020-08-07 06:14:14 +0100

    Update buzzword.c
    Add arrays of buzzwords.

--- buzzword.c
+++ buzzword.c
@@ -9,5 +9,10 @@
  int main(int argc, const char * argv[])
  {
    printf("Buzzword Generator\nBased on a programme from Creative Computing, Morristown, New Jersey\n\n\nThis programme prints highly acceptable phrases in 'educator-speak' that you can work into reports and speeches. Whenever a question mark is printed, type a 'Y' for another phrase or 'N' to quit.\n\n\nHere's the first phrase:\n");
+
+   const char * adjectives1[] = {"ability", "basal", "behavioral", "child-centered", "differentiated", "discovery", "flexible", "heterogeneous", "homogenous", "manipulative", "modular", "tavistock", "individualized" };
+   const char * adjectives2[] = {"learning", "evaluative", "objective", "cognitive", "enrichment", "scheduling", "humanistic", "integrated", "non-graded", "training", "vertical age", "motivational", "creative"};
+   const char * nouns[] = {"grouping", "modification", "accountability", "process", "core curriculum", "algorithm", "performance", "reinforcement", "open classroom", "resource", "structure", "facility", "environment"};
+
    return 0;
  }
