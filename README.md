138e18bc9ecf987a533049cf100a8769240ca9ab
Laurence Scotford <laurencescotford@gmail.com>
2020-08-10 07:29:45 +0100

    Create final version
    Update buzzword.c to accept an optional command line argument or default to single phrases with prompt if no argument is given
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # c-games-buzzword
-A conversion to C of the Buzzword game from David Ahl's Basic Computer Games
+A conversion to C of the Buzzword game from David Ahl's *Basic Computer Games*. Buzzword prints highly acceptable phrases in 'educator-speak' that you can work into reports and speeches.
+## How to compile the program
+Buzzword consists of a single file, *buzzword.c*. Simply compile this to create an executable.
+## How to use Buzzword
+Buzzword takes an optional command line argument, an integer between 1 and 99 that indicates how many phrases you would like to generate. For example, if you compiled the program to create an executable called "buzzword", you could generate 5 phrases with:
+```
+buzzword 5
+```
+If you run the program without a command line argument, it will display a single phrase and then display a question mark and wait for your input. Entering **y** or **Y** then hitting the enter or return key will display another phrase. Entering any other character and hitting enter or return will end the program.
--- buzzword.c
+++ buzzword.c
@@ -7,11 +7,49 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <time.h>
+ #include <errno.h>
+
+ // Function declarations
+ void displayUsageAndExit();
 
   int main(int argc, const char * argv[]) {
 
-   // Display the instructions
-   printf("Buzzword Generator\nBased on a programme from Creative Computing, Morristown, New Jersey\n\n\nThis programme prints highly acceptable phrases in 'educator-speak' that you can work into reports and speeches. Whenever a question mark is printed, type a 'Y' for another phrase or 'N' to quit.\n\n\nHere's the first phrase:\n");
+  int numberOfPhrases = 1;  // The number of phrases to display
+
+  // The mode of operation: 0 = single phrase then prompt for more, 1 = display number of phrases requested in the command line arg
+  int mode = 1;
+
+  if (argc == 1) {
+     // There was no argument so set mode to single phrase with prompt
+     mode = 0;
+   } else if(argc != 2) {
+     // An incorrect number of arguments was given, so quit with usage message
+     displayUsageAndExit();
+   } else {
+     // A single argument was given, so attempt to convert it to an integer
+     errno = 0;
+     char * inputStr;
+     long inputNum = strtol(argv[1], &inputStr, 10);
+
+     if(errno == ERANGE || *inputStr != '\0' || inputNum < 1 || inputNum > 99) {
+       // The argument could not be interpreted as an integer in the required range, so quit with usage message
+       displayUsageAndExit();
+     }
+
+     numberOfPhrases = (int) inputNum;
+   }
+
+   // Display the introduction and instructions if required
+   printf("Buzzword Generator\nBased on a programme from Creative Computing, Morristown, New Jersey\n\n\nThis programme prints highly acceptable phrases in 'educator-speak' that you can work into reports and speeches.");
+
+   if (mode == 0) {
+     printf(" Whenever a question mark is printed, type a 'Y' for another phrase or 'N' to quit.\n\n\nHere's the first phrase:\n");
+   }
+   else if (numberOfPhrases == 1) {
+     printf("\n\n\nHere's your phrase:");
+   } else {
+     printf("\n\n\nHere are your %i phrases", numberOfPhrases);
+   }
 
    // Create three lists of words
    const char * adjectives1[] = {"ability", "basal", "behavioral", "child-centered", "differentiated", "discovery", "flexible", "heterogeneous", "homogenous", "manipulative", "modular", "tavistock", "individualized" };
@@ -26,14 +64,23 @@
 
    // Display random phrases while user keeps requesting them
    do {
-     printf("\n%s %s %s\n\n", adjectives1[rand() % 13], adjectives2[rand() % 13], nouns[rand() % 13]);
+     for (int i = 0; i < numberOfPhrases; i++) {
+       printf("\n%s %s %s\n\n", adjectives1[rand() % 13], adjectives2[rand() % 13], nouns[rand() % 13]);
+     }
 
-     printf("?");
-     fgets(rawInput, sizeof(rawInput) / sizeof(rawInput[0]), stdin);
-     sscanf(rawInput, " %c", &input);
-   } while (input == 'y' || input == 'Y');
+     if (mode == 0) {
+       printf("?");
+       fgets(rawInput, sizeof(rawInput) / sizeof(rawInput[0]), stdin);
+       sscanf(rawInput, " %c", &input);
+     }
+   } while (mode == 0 && (input == 'y' || input == 'Y'));
 
    printf("Come back when you need help with another report!");
 
    return EXIT_SUCCESS;
+ }
+
+ void displayUsageAndExit() {
+   printf("\nUsage: BUZZWORD number_of_phrases\nWhere number of phrases is an optional argument - a whole number from 1 to 99");
+   exit(EXIT_FAILURE);
  }
